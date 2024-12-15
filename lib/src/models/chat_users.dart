8f80036c9841521811eb5e24e6dbc04611fa5e16
KarthiDreamr <karthidreamr@gmail.com>
2024-12-15 21:46:06 +0530

    moved files inside src

--- lib/src/models/chat_users.dart
+++ lib/src/models/chat_users.dart
@@ -1,12 +1,12 @@
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 
 class ChatUsers {
-  static const types.User currentUser = types.User(
+  static types.User currentUser = types.User(
     id: 'karthidreamr',
     firstName: 'KarthiDreamr',
   );
 
-  static const types.User aiUser = types.User(
+  static types.User aiUser = types.User(
     id: 'gemini-ai',
     firstName: 'Gemini',
   );
