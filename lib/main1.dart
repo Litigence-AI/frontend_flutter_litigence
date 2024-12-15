7cc9a03cff1b731587f5ea1ad69089245298c120
karthidreamr <karthidreamr@gmail.com>
2024-12-15 15:06:50 +0000

    added alternate main function

---
+++ lib/main1.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart' as dotenv;
+import 'pages/chat_page.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // Pass all uncaught "fatal" errors from the framework to Crashlytics
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
+  await dotenv.dotenv.load();
+  initializeDateFormatting().then((_) => runApp(const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) => const MaterialApp(
+        home: Directionality(
+          textDirection: TextDirection.ltr,
+          child: ChatPage(),
+        ),
+      );
+}
