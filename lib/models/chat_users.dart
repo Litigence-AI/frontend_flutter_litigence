2f039075d4e46821eff4e45ff701d58c3b316741
karthidreamr <karthidreamr@gmail.com>
2024-12-15 15:07:13 +0000

    added users & AI type

---
+++ lib/models/chat_users.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
+
+class ChatUsers {
+  static const types.User currentUser = types.User(
+    id: 'karthidreamr',
+    firstName: 'KarthiDreamr',
+  );
+
+  static const types.User aiUser = types.User(
+    id: 'gemini-ai',
+    firstName: 'Gemini',
+  );
+}
