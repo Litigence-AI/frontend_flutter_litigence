4c79e152f61d8cc03c4aff224a0fd3d845917078
karthidreamr <karthidreamr@gmail.com>
2025-01-11 10:37:01 +0000

    refactor: moved login screen to auth folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:Litigence/login_screen.dart';
+import 'package:Litigence/authentication/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:shared_preferences/shared_preferences.dart';
