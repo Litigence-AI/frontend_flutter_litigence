7a0ce863c170c96ec1b3fe443e61485cc1004f63
surendar-05 <surendarrockzz7@gmail.com>
2025-01-17 21:39:51 +0530

    fix: authScreen responsiveness

--- lib/authentication/auth_screen.dart
+++ lib/authentication/auth_screen.dart
@@ -99,134 +99,143 @@ class _AuthScreenState extends State<AuthScreen> {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // Time status bar (9:41) would be handled by the system
-
-              // Logo and Lady Justice image
-              Image.asset(
-                'assets/lady_justice.png',
-                height: 120,
-              ),
-
-              const SizedBox(height: 24),
-
-              // Title text
-              const Text(
-                'Bridging knowledge gaps\nfor a fairer world',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-
-              const SizedBox(height: 48),
-
-              // Google Sign In Button
-              ElevatedButton(
-                onPressed: signInWithGoogle,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  foregroundColor: Colors.black,
-                  minimumSize: const Size(double.infinity, 50),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                      'assets/logo/google.png',
-                      height: 24,
-                    ),
-                    const SizedBox(width: 12),
-                    const Text(
-                      'Continue with Google',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-
-              const SizedBox(height: 16),
-
-              // Phone Number Button
-              ElevatedButton(
-                onPressed: () {
-                  // Implement Phone Number Sign In
-                  context.go('/otpAuthScreen');
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  foregroundColor: Colors.black,
-                  minimumSize: const Size(double.infinity, 50),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                ),
-                child: const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.phone_android,
-                      size: 24,
-                      color: Color.fromARGB(217, 14, 14, 14),
-                    ),
-                    SizedBox(width: 12),
-                    Text(
-                      'Continue with Phone number',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-
-              const Spacer(),
-
-              // Footer links
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+        child: Center(
+          // Ensures the content is centered on larger screens
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(maxWidth: 400),
+            // Limit the width for web
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: Column(
+                 // crossAxisAlignment: CrossAxisAlignment.spacebetween,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  TextButton(
-                    onPressed: () {
-                      _launchURL(
-                          'https://litigence-ai.github.io/privacy-policy/');
-                    },
-                    child: const Text(
-                      'Privacy policy',
-                      style: TextStyle(
-                        color: Colors.white70,
-                      ),
+
+                  const Text(
+                    "Litigence AI",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                  const SizedBox(width: 24),
-                  TextButton(
-                    onPressed: () {
-                      _launchURL(
-                          'https://litigence-ai.github.io/Terms-of-service/');
-                    },
-                    child: const Text(
-                      'Terms of service',
-                      style: TextStyle(
-                        color: Colors.white70,
-                      ),
+
+                  // Logo and Lady Justice image
+
+                  Image.asset(
+                    'assets/lady_justice.png',
+                    height: 120,
+                  ),
+
+                  const Text(
+                    'Bridging knowledge gaps\nfor a fairer world',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
                     ),
+                  ),
+
+                  Column(
+                    children: [
+                      ElevatedButton(
+                        onPressed: signInWithGoogle,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          foregroundColor: Colors.black,
+                          minimumSize: const Size(300, 50),
+                          // Set fixed width for buttons
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              'assets/logo/google.png',
+                              height: 24,
+                            ),
+                            const SizedBox(width: 12),
+                            const Text(
+                              'Continue with Google',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height:20),
+
+                      ElevatedButton(
+                        onPressed: () {
+                          context.go('/otpAuthScreen');
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          foregroundColor: Colors.black,
+                          minimumSize: const Size(300, 50),
+                          // Set fixed width for buttons
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                        child: const Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.phone_android,
+                              size: 24,
+                              color: Color.fromARGB(217, 14, 14, 14),
+                            ),
+                            SizedBox(width: 12),
+                            Text(
+                              'Continue with Phone number',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  )
+
+                  ,
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextButton(
+                        onPressed: () {
+                          _launchURL(
+                              'https://litigence-ai.github.io/privacy-policy/');
+                        },
+                        child: const Text(
+                          'Privacy policy',
+                          style: TextStyle(color: Colors.white70),
+                        ),
+                      ),
+
+                      TextButton(
+                        onPressed: () {
+                          _launchURL(
+                              'https://litigence-ai.github.io/Terms-of-service/');
+                        },
+                        child: const Text(
+                          'Terms of service',
+                          style: TextStyle(color: Colors.white70),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
+            ),
           ),
         ),
       ),
