a03aa2e063684e3fbf7f8f2ad48b6284db8c025b
karthidreamr <karthidreamr@gmail.com>
2025-02-03 10:42:17 +0000

    fix: handled web only in url stratagy

---
+++ lib/url_strategy_config_stub.dart
@@ -0,0 +1,4 @@
+// url_strategy_config_stub.dart
+void configureUrlStrategy() {
+  // Do nothing on non-web platforms.
+}
---
+++ lib/url_strategy_config_web.dart
@@ -0,0 +1,6 @@
+// url_strategy_config_web.dart
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void configureUrlStrategy() {
+  setUrlStrategy(PathUrlStrategy());
+}
